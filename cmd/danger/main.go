6b73fbca3a27fdd42531622175d42613f47d12fa
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-27 13:32:55 +0800

    add networks and 1st genesis iteration

---
+++ cmd/danger/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/CosmWasm/wasmd/app"
+	"github.com/notional-labs/danger/cmd/danger/cmd"
+
+	"cosmossdk.io/log"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+
+	if err := svrcmd.Execute(rootCmd, "DANGERD", app.DefaultNodeHome); err != nil {
+		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
+		os.Exit(1)
+	}
+}
